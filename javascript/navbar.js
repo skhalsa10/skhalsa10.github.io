411ba0748a6081ad8539b7017377f1c1d528c270
Siri Khalsa <sirikhalsa@Siris-MacBook-Pro.local>
2024-11-29 19:05:44 -0800

    refactor using bulma 1.0

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,9 +1,9 @@
-//mobile menu
+
 
 const burgerIcon = document.querySelector('#burger');
 const navbarMenu = document.querySelector('#navbar-menu-links');
 
-burgerIcon.addEventListener('click', ()=>{
+burgerIcon.addEventListener('click', () => {
     navbarMenu.classList.toggle('is-active');
     burgerIcon.classList.toggle('is-active');
 });
